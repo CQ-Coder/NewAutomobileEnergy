3981c7a7b1a3700a8f9e8a51acc0a65751169e96
CQ-Coder <CQ-Coder@163.com>
2018-03-10 00:03:20 +0800

    更改了首页list页面，页面结构基本不会动，以后仅根据具体UI图进行css更改

--- car-boot/gulpfile.js
+++ car-boot/gulpfile.js
@@ -16,82 +16,82 @@ var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 
 gulp.task('clean', function (cb) {
-    return del(['dist/*'], cb);
+    return del(['dist/front/*'], cb);
 });
 
 gulp.task('html', function () {
-    return gulp.src('src/*.html')
-        .pipe(gulp.dest('dist'));
+    return gulp.src('src/front/*.html')
+        .pipe(gulp.dest('dist/front'));
 });
 
 gulp.task('image', function () {
-    return gulp.src('src/imgs/*')
-      .pipe(gulp.dest('dist/imgs'))
+    return gulp.src('src/front/imgs/*')
+      .pipe(gulp.dest('dist/front/imgs'))
       //.pipe(browserSync.stream());
 });
 
 gulp.task('cssImage', function () {
-    return gulp.src('src/css/images/*')
-      .pipe(gulp.dest('dist/css/images'))
+    return gulp.src('src/front/css/images/*')
+      .pipe(gulp.dest('dist/front/css/images'))
       //.pipe(browserSync.stream());
 });
 gulp.task('json', function () {
-    return gulp.src('src/data/*.json')
+    return gulp.src('src/front/data/*.json')
       .pipe(gulp.dest('dist/data'))
       //.pipe(browserSync.stream());
 });
 gulp.task('scss', function () {
-    return gulp.src('src/scss/*.scss')
+    return gulp.src('src/front/scss/*.scss')
       .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       // .pipe(gulp.dest('dist/css'))
       .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(sourcemaps.write('./maps'))
-      .pipe(gulp.dest('dist/css'))
-      // .pipe(gulp.dest('src/css'))
+      .pipe(gulp.dest('dist/front/css'))
+      // .pipe(gulp.dest('src/front/css'))
       //.pipe(browserSync.stream());
 
 });
 //css
 gulp.task('css', function() {
-  return gulp.src('src/css/*.css')
-    .pipe(gulp.dest('dist/css'))
+  return gulp.src('src/front/css/*.css')
+    .pipe(gulp.dest('dist/front/css'))
     //.pipe(browserSync.stream());
 });
 //新增common
 //common css 
 gulp.task('commonCss', function() {
-  return gulp.src('src/css/common/*.css')
-    .pipe(gulp.dest('dist/css/common'))
+  return gulp.src('src/front/css/common/*.css')
+    .pipe(gulp.dest('dist/front/css/common'))
     //.pipe(browserSync.stream());
 });
 //common js 
 gulp.task('commonJs', function() {
-  return gulp.src('src/js/common/*.js')
-    .pipe(gulp.dest('dist/js/common'))
+  return gulp.src('src/front/js/common/*.js')
+    .pipe(gulp.dest('dist/front/js/common'))
     //.pipe(browserSync.stream());
 });
 
 gulp.task('js', function () {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src('src/front/js/**/*.js')
         // .pipe(sourcemaps.init())
         // .pipe(concat('main.js'))
         // .pipe(gulp.dest('dist/js'))
         // .pipe(rename({suffix: '.min'}))
         // .pipe(uglify())
         // .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest('dist/front/js'))
         //.pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/*.html', ['html']);
-    gulp.watch('src/data/*.json', ['json']);
-    gulp.watch('src/imgs/*', ['image']);
-    gulp.watch('src/scss/**/*.scss', ['scss']);
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('src/**/*.css', ['css']);
+    gulp.watch('src/front/*.html', ['html']);
+    gulp.watch('src/front/data/*.json', ['json']);
+    gulp.watch('src/front/imgs/*', ['image']);
+    gulp.watch('src/front/scss/**/*.scss', ['scss']);
+    gulp.watch('src/front/js/**/*.js', ['js']);
+    gulp.watch('src/front/**/*.css', ['css']);
 });
 
 //static server
@@ -101,10 +101,10 @@ gulp.task('staticServer', ['html','json' ,'image', 'cssImage', 'scss', 'css','co
             baseDir: "./dist",
             directory: true
         },
-        startPath: "index.html"
+        startPath: "front/index.html"
     });
 
-    gulp.watch('./src/*.html').on('change', browserSync.reload);
+    gulp.watch('./src/front/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('server', ['staticServer']);
